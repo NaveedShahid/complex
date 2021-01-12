7d09f39471decff8b138d01a655cf3b51f73a1cb
NaveedShahid <naveedshahid12@gmail.com>
2021-01-12 17:03:07 +0500

    changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const Todo = () => {
   return (
     <div>
-      Im some other page!
+            Im some other page!
       <Link to="/">Go back home</Link>
     </div>
   );
 };
+
+export default Todo;
